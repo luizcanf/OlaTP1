427b0143ce68e421e7557635bb9f4eef4ac4a0e6
Geleia <luizcanf@gmail.com>
2025-03-18 15:21:29 -0300

    Update servidor.js
    Nomes dos estudantes de PdC da turma de 2024

--- servidor.js
+++ servidor.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-    nomes = ['Adrielle', 'Aigor', 'Angelo', 'Beatriz', 'Carlos Augusto', 'Daniel Gonçalves', 'Daniel Leaubon', 'Davi Jorge', 'Davi Trajano', 'Diego', 'Eduardo', 'Eliel', 'Evellin', 'Hugo', 'Isabelly', 'João Pedro', 'Jonathan', 'Júlia', 'Kauan', 'Maria Eduarda', 'Nathaly', 'Rafhael', 'Sabrina', 'Thales', 'Cesar', 'Cesinha', 'Geleia']
+    nomes = ['Abel', 'Analiz', 'Arthur', 'Arthur', 'Cassiano', 'Daniel', 'Davi', 'Davi', 'Edgar', 'Elisa', 'Eloá', 'Esmeralda', 'Giovana', 'Hadassa', 'Hillary', 'João Marcos', 'João Pedro', 'Juliana', 'Kamily', 'Kayky', 'Lailla', 'Leonardo', 'Lorenna', 'Pilar', 'Mariana', 'Matheus', 'Miguel', 'Matheus', 'Miguel', 'Pedro', 'Raiane', 'Raquel', 'Rhaniery', 'Cesar', 'Cesinha', 'Geleia']
     i = getRandomInt(0, nomes.length-1)
     response.send('Olá, você é '+nomes[i]+'?')
 })
