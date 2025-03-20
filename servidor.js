7b709abd5ca36af07194689f5ba4d998e9b29c30
Geleia <luizcanf@gmail.com>
2025-03-20 10:03:01 -0300

    Novas rotas com métodos GET, POST e DELETE

--- servidor.js
+++ servidor.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
+let nomes = ['Abel', 'Analiz', 'Arthur', 'Arthur', 'Cassiano', 'Daniel', 'Davi', 'Davi', 'Edgar', 'Elisa', 'Eloá', 'Esmeralda', 'Giovana', 'Hadassa', 'Hillary', 'João Marcos', 'João Pedro', 'Juliana', 'Kamily', 'Kayky', 'Lailla', 'Leonardo', 'Lorenna', 'Pilar', 'Mariana', 'Matheus', 'Miguel', 'Matheus', 'Miguel', 'Pedro', 'Raiane', 'Raquel', 'Rhaniery', 'Cesar', 'Cesinha', 'Geleia']
+
 app.get('/', (request, response) => {
-    nomes = ['Abel', 'Analiz', 'Arthur', 'Arthur', 'Cassiano', 'Daniel', 'Davi', 'Davi', 'Edgar', 'Elisa', 'Eloá', 'Esmeralda', 'Giovana', 'Hadassa', 'Hillary', 'João Marcos', 'João Pedro', 'Juliana', 'Kamily', 'Kayky', 'Lailla', 'Leonardo', 'Lorenna', 'Pilar', 'Mariana', 'Matheus', 'Miguel', 'Matheus', 'Miguel', 'Pedro', 'Raiane', 'Raquel', 'Rhaniery', 'Cesar', 'Cesinha', 'Geleia']
     i = getRandomInt(0, nomes.length-1)
     response.send('Olá, você é '+nomes[i]+'?')
+})
+
+app.get('/nomes', (request, response) => {
+    response.send('Lista dos nomes: '+nomes)
+})
+
+app.post('/apresente-se', (request, response) => {
+    let nome = request.body.nome
+    
+    if (nomes.includes(nome)) {
+        response.send('Olá, '+nome+'! Seu nome já estava na lista.')
+    } else {
+        nomes.push(nome)
+        response.send('Olá, '+nome+'! Acabei de incluir seu nome na lista.')
+    }
+})
+
+app.delete('/reset', (request, response) => {
+    let total = request.body.total
+    
+    if (total == "verdadeiro") {
+        nomes = ['Abel', 'Analiz', 'Arthur', 'Arthur', 'Cassiano', 'Daniel', 'Davi', 'Davi', 'Edgar', 'Elisa', 'Eloá', 'Esmeralda', 'Giovana', 'Hadassa', 'Hillary', 'João Marcos', 'João Pedro', 'Juliana', 'Kamily', 'Kayky', 'Lailla', 'Leonardo', 'Lorenna', 'Pilar', 'Mariana', 'Matheus', 'Miguel', 'Matheus', 'Miguel', 'Pedro', 'Raiane', 'Raquel', 'Rhaniery', 'Cesar', 'Cesinha', 'Geleia']
+        response.send('Lista de nomes resetada para a lista inicial.')
+    } else {
+        let nome = request.body.nome
+        const index = nomes.indexOf(nome)
+        if (index > -1) {
+            nomes.splice(index, 1)
+            response.send('Nome "'+nome+'" removido da lista.')
+        } else {
+            response.send('Nome "'+nome+'" não encontrado na lista.')
+        }
+    }
 })
 
 function getRandomInt(min, max) {
