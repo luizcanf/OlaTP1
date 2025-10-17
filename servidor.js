4df9ab9d67f10800b3f91f8a2ffc88693d1fd1fa
Geleia <luizcanf@gmail.com>
2025-10-16 22:22:40 -0300

    Mudança na ordem do CORS no stack de middlewares.

--- servidor.js
+++ servidor.js
@@ -1,9 +1,9 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
 const port = process.env.PORT || 3000;
 
 //let nomesPdC2024 = ['Abel', 'Analiz', 'Arthur', 'Arthur', 'Cassiano', 'Daniel', 'Davi', 'Davi', 'Edgar', 'Elisa', 'Eloá', 'Esmeralda', 'Giovana', 'Hadassa', 'Hillary', 'João Marcos', 'João Pedro', 'Juliana', 'Kamily', 'Kayky', 'Lailla', 'Leonardo', 'Lorenna', 'Pilar', 'Mariana', 'Matheus', 'Miguel', 'Matheus', 'Miguel', 'Pedro', 'Raiane', 'Raquel', 'Rhaniery', 'Cesar', 'Cesinha', 'Geleia']
