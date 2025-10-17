6ec34a6e2847de557ddd85af58dbc2aa1aff43ec
Geleia <luizcanf@gmail.com>
2025-10-16 22:29:42 -0300

    Ativando preflight do OPTIONS no CORS

--- servidor.js
+++ servidor.js
@@ -2,6 +2,7 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 app.use(cors())
+app.options('*', cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const port = process.env.PORT || 3000;
